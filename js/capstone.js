5e0f4f173ded46b04e7d9fb008eb21a384d40f9f
Vikash Arya <vikasarya457@gmail.com>
2020-09-19 14:48:50 +0530

    nuclearPower-edited

---
+++ js/capstone.js
@@ -0,0 +1,11 @@
+function pollfunction(){
+	if(confirmation.checked){
+		window.alert("Thakyou for your opinion.....successfully submitted");
+	}
+	else{
+		window.alert("please fill all the details before submition");
+	}
+}
+function calanderalert(){
+	window.alert('To access ***GOOGLE calander*** click on site logo');
+}
